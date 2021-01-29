b0cb5c7c9130edceff6f014b018730d0f314d391
Shun Furukawa <furukawa9312@gmail.com>
2021-01-29 20:30:22 +0900

    Modify Content

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h1>Hello World!</h1>
+    <h2>Article Index</h2>
+    <!-- <div>{{ home }}</div> -->
     <div v-for="article in articles" :key="article.slug">
       <nuxt-link :to="'/articles/'+ article.slug">{{article.title}} {{article.date}}</nuxt-link>
     </div>
@@ -10,8 +12,9 @@
 <script>
   export default {
     async asyncData ({ $content }) {
-      const query = await $content('articles')
+      const query = await $content('articles', { deep: true })
       const articles = await query.fetch()
+      // const home = await $content('home' || 'index').fetch()
 
       return {
         articles
