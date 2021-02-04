7cc8fe3b14ea955126fa4b1778a3195967edff1a
Shun Furukawa <furukawa9312@gmail.com>
2021-02-04 13:55:47 +0900

    ADD components table of content

---
+++ components/TableOfContent.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <h3>Table Of Content</h3>
+    <div>
+      <ul>
+        <li
+          v-for='link of toc'
+          :key='link.id'
+        >
+          <nuxt-link :to="`#${link.id}`">{{ link.text }}</nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    props: ['toc']
+  };
+</script>
