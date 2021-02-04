cf13e9cca6544d1e01e0181910696abc9566e0b8
Shun Furukawa <furukawa9312@gmail.com>
2021-02-04 11:07:12 +0900

    ADD favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,9 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Sample Nuxt Content Description' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   }
 }
